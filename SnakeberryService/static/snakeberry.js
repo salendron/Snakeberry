1f8ef39317b9802e09f650973465ba3c13f7fbda
Jakob Voss <jakob@nichtich.de>
2013-04-06 22:11:49 +0200

    show currently playing radio

--- SnakeberryService/static/snakeberry.js
+++ SnakeberryService/static/snakeberry.js
@@ -5,7 +5,10 @@
 function SnakeberryClient(args) {
     this.baseURL = args.baseURL;
     this.radiosList = $(args.radiosList);
+    this.nowplaying = null;
+    this.radios = { };
 
+    this.updateCurrentRadio();
     this.updateRadios();
 }
 
@@ -19,9 +22,34 @@ SnakeberryClient.prototype.updateRadios = function() {
         $.each(radios,function(k,v){
             me.radios[v.RadioId] = v.DisplayName;
             var a=$('<a>').text(v.DisplayName);
-            var li=$('<li>',{id:'radio-'+v.RadioId}).append(a);
+            var id = me.hashCode(v.StreamUrl); // MediaURI ?!
+            var li=$('<li>',{id:'radio-'+id}).append(a);
+            if (me.nowplaying && me.nowplaying == v.RadioId) {
+                li.addClass('selected');
+            }
             me.radiosList.append(li);
         });
     });
+};
+
+SnakeberryClient.prototype.updateCurrentRadio = function() {
+    var me = this;
+    $.ajax( this.baseURL + 'radio/nowplaying' ).success(function(json) {
+        var response = json.ResponseData;
+        var id = me.hashCode(response.MediaURI);
+        me.radiosList.find('.selected').removeClass('selected');
+        $('#radio-'+id).addClass('selected');
+    });
+};
+
+// implements Java.lang.String.hashCode()
+SnakeberryClient.prototype.hashCode = function(s){
+    var hash = 0;
+    if (s == undefined || !s.length) return hash;
+    for (i = 0; i < s.length; i++) {
+        hash = ((hash<<5)-hash) + s.charCodeAt(i);
+        hash = hash & hash; // 32bit int
+    }
+    return hash;
 };
 
