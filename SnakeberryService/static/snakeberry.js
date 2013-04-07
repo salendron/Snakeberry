0b94fd01289f6fe4ca55b42f46757c52d50fac57
Jakob Voss <jakob@nichtich.de>
2013-04-07 21:56:08 +0200

    web client implements radio selection

--- SnakeberryService/static/snakeberry.js
+++ SnakeberryService/static/snakeberry.js
@@ -2,48 +2,23 @@
  * Snakeberry JavaScipt client
  */
 
-function SnakeberryClient(args) {
-    this.baseURL = args.baseURL;
-    this.radiosList = $(args.radiosList);
-    this.nowplaying = null;
-    this.radios = { };
-
-    this.updateCurrentRadio();
-    this.updateRadios();
+// A radio station
+function Radio(r) {
+    this.name   = r.DisplayName;
+    this.stream = r.MediaURI ? r.MediaURI : r.StreamUrl;
+    this.hash   = this.hashCode(this.stream);
+    this.id     = r.RadioId;
 }
 
-SnakeberryClient.prototype.updateRadios = function() {
-    var me = this;
-    $.ajax( this.baseURL + 'radios' ).success(function(json) {
-        // TODO: error handling
-        var radios = json.ResponseData.Radios; // TODO: catch
-        me.radiosList.empty();
-        me.radios = {};
-        $.each(radios,function(k,v){
-            me.radios[v.RadioId] = v.DisplayName;
-            var a=$('<a>').text(v.DisplayName);
-            var id = me.hashCode(v.StreamUrl); // MediaURI ?!
-            var li=$('<li>',{id:'radio-'+id}).append(a);
-            if (me.nowplaying && me.nowplaying == v.RadioId) {
-                li.addClass('selected');
-            }
-            me.radiosList.append(li);
-        });
-    });
-};
-
-SnakeberryClient.prototype.updateCurrentRadio = function() {
-    var me = this;
-    $.ajax( this.baseURL + 'radio/nowplaying' ).success(function(json) {
-        var response = json.ResponseData;
-        var id = me.hashCode(response.MediaURI);
-        me.radiosList.find('.selected').removeClass('selected');
-        $('#radio-'+id).addClass('selected');
-    });
-};
+Radio.prototype.listItem = function(player) {
+    var radio = this;
+    var a  = $('<a>').text(radio.name);
+    var li = $('<li>',{id:'radio'+radio.hash}).append(a);
+    return li;
+}
 
 // implements Java.lang.String.hashCode()
-SnakeberryClient.prototype.hashCode = function(s){
+Radio.prototype.hashCode = function(s) {
     var hash = 0;
     if (s == undefined || !s.length) return hash;
     for (i = 0; i < s.length; i++) {
@@ -51,5 +26,96 @@ SnakeberryClient.prototype.hashCode = function(s){
         hash = hash & hash; // 32bit int
     }
     return hash;
+};
+
+// A Radio player client
+function SnakeberryClient(args) {
+    var player = this;
+    this.baseURL = args.baseURL;
+    this.radiosList = $(args.radiosList);
+    this.volumeControl = $(args.volumeControl);
+    this.volumeControl.change(function(){
+//        player.setVolume($(this).val());
+    });
+    this.playing = null;
+    this.radios = { };
+    this.updateRadios();
+    this.updatePlaying();
+//    this.updateVolume();
+}
+
+SnakeberryClient.prototype.playRadio = function(radio) {
+    var player = this;
+    $.ajax( player.baseURL + 'radio/play/' + radio.id ).success(function(json){
+        player.playing = radio;
+        player.showPlaying();
+    });
+};
+
+SnakeberryClient.prototype.stopPlaying = function() {
+    var player = this;
+    $.ajax( player.baseURL + 'radio/stop' ).success(function(json){
+        player.playing = null;
+        player.showPlaying();
+    })
+};
+
+SnakeberryClient.prototype.showPlaying = function() {
+    var player = this;
+    player.radiosList.find('.active').each(function(){
+        $(this).removeClass('active');
+        $(this).off('click');
+        var hash = $(this).attr('id').substring(5);
+        var radio = player.radios[hash];
+        $(this).click(function(){ player.playRadio(radio) });
+    });
+    if (player.playing) {
+        $('#radio'+player.playing.hash).addClass('active').click(
+            function(){player.stopPlaying()}
+        );
+    }
+};
+
+SnakeberryClient.prototype.updateRadios = function() {
+    var player = this;
+    $.ajax( player.baseURL + 'radios' ).success(function(json) {
+        player.radiosList.empty();
+        player.radios = {};
+        $.each(json.ResponseData.Radios,function(k,r){
+            var radio = new Radio(r);
+            player.radios[radio.hash] = radio;
+            var li = radio.listItem();
+            if (player.playing && player.playing.hash == radio.hash) {
+                li.addClass('active');
+                li.click(function(){player.stopPlaying()});
+            } else {
+                li.click(function(){player.playRadio(radio)});
+            }
+            player.radiosList.append(li);
+        });
+    });
+};
+
+SnakeberryClient.prototype.updatePlaying = function() {
+    var player = this;
+    $.ajax( player.baseURL + 'radio/nowplaying' ).success(function(json) {
+        player.playing = new Radio(json.ResponseData);
+        player.showPlaying();
+    });
+};
+
+SnakeberryClient.prototype.updateVolume = function() {
+    var player = this;
+    $.ajax( player.baseURL + 'getvolume' ).success(function(json) {
+        var volume = json.ResponseData;
+        player.volumeControl.val(volume);
+    });
+};
+
+SnakeberryClient.prototype.setVolume = function(volume) {
+    var player = this;
+    $.ajax( player.baseURL + 'setvolume/' + volume ).success(function(json) {
+        player.volumeControl.val(volume);
+    });
 };
 
